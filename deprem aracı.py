a18ceaac3c454112b8e918285250ee5f9bb3ebcb
Soulfly0671 <87572847+kayradmrc0671@users.noreply.github.com>
2022-02-01 22:11:31 +0300

    Add files via upload
    güncel deprem göstergeci

---
+++ deprem aracı.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+print("#############################################")
+print("GÜNCEL DEPREM GÖRÜNTELİYİCİ")
+print("Coder:By soulfly.py")
+print("##############################################")
+
+url = "http://www.koeri.boun.edu.tr/scripts/lst0.asp"
+
+response = requests.get(url)
+
+content = response.content
+
+soup = BeautifulSoup(content, "html.parser")
+
+pre = soup.find("pre")
+
+text = pre.text
+
+liste = text.split("\n")
+
+print("\n.".join(liste[4:8]))
